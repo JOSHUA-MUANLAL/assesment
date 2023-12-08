4155c62947fa2de87a1d0fb008719d57c6b90945
JOSHUA-MUANLAL <105428544+JOSHUA-MUANLAL@users.noreply.github.com>
2023-12-08 21:10:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.set('views', viewsPath);
 
 
 // create connection
-var con = mysql.createPool({
+var con = mysql.createConnection({
     connectionLimit: 10,
     host: "sql12.freesqldatabase.com",
     port: 3306,
